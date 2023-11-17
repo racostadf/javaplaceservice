3c2090e68a02ef9999a8dae5ed371a11572929ec
Rogerio Costa <RACOSTADF@GMAIL.COM>
2023-11-17 10:37:27 -0300

    feature: teste de adicao de comentario

--- src/main/java/br/com/rogerio/placeservice/domain/PlaceService.java
+++ src/main/java/br/com/rogerio/placeservice/domain/PlaceService.java
@@ -3,6 +3,7 @@ package br.com.rogerio.placeservice.domain;
 import br.com.rogerio.placeservice.api.PlaceRequest;
 import reactor.core.publisher.Mono;
 
+//Entendendo como o services e os mapper funcionam
 public class PlaceService {
     private PlaceRepository placeRepository;
 
